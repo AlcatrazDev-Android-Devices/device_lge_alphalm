48a37cda96d8d81a30757d90902ead88024b5121
Alcatraz323 <alcatraz32323@gmail.com>
2024-07-25 09:34:45 +0800

    alphalm: overlay: merge overlay to aosp format

--- device.mk
+++ device.mk
@@ -20,8 +20,7 @@ TARGET_SCREEN_HEIGHT := 2248
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
